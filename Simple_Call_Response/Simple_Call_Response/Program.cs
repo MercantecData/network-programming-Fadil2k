9036663d98b1edd5599512bb19a6d9c6411e3487
Fadil <mail@fadil.dk>
2020-05-25 10:53:42 +0200

    Created C# Simple Call Response Project

---
+++ Simple_Call_Response/Simple_Call_Response/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Simple_Call_Response
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
